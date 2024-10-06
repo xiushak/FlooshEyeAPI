99b7a2fa56bee831393e1f917142e9d09d5b803c
xiushak <xiebrandon@gmail.com>
2024-10-06 13:10:40 -0700

    first commit

---
+++ src/main/java/com/xiushak/floosheye/FloosheyeApplication.java
@@ -0,0 +1,13 @@
+package com.xiushak.floosheye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloosheyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloosheyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiushak/floosheye/FloosheyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiushak.floosheye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloosheyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
